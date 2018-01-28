16b94457a35dcb71eb38a6b7e780cf236fb4d10f
ryangadz@msn.com <ryangadz@msn.com>
2018-01-28 14:32:58 -0800

    delete commented code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 const http = require("http");
 var debug = require('debug');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 var express = require('express');
 var path = require('path');
 
@@ -19,43 +19,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-// var server = app.listen(app.get('port'), function () {
-//     debug('Express server listening on port ' + server.address().port);
-// });
 
-// const handler = (req, res) => {
-//     console.log('Server recieved request. ');
-//     res.end('Hello World! from vs code and github! yay!!');
-// };
-
-//const server = http.createServer(handler);
-
-
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function (err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
-
-// server.listen(port, err => {
-//     if (err){
-//         console.log(err);
-//     }
-//     else {
-//         console.log('Server listening on port: ${port}');
-//     }
-// });
