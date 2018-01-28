2005583bae6562f28b046b9baf79b198553000c2
ryangadz@msn.com <ryangadz@msn.com>
2018-01-28 14:43:18 -0800

    added error catch

--- app.js
+++ app.js
@@ -19,4 +19,39 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(1337, () => console.log('Example app listening on port 3000!'))
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+//app.set('port', process.env.PORT || 1337);
+
+var server = app.listen(app.get('port'), function () {
+    debug('Express server listening on port ' + server.address().port);
+});
