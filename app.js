bf3b34d61d995a5a70444d7aedf4f607b120bc25
ryangadz@msn.com <ryangadz@msn.com>
2018-01-28 22:43:12 -0800

    logger and cookie-parser

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var debug = require('debug');
 const port = process.env.PORT || 1337;
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
@@ -13,12 +15,14 @@ var app = express();
 
 var routes = require('./routes/index');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
